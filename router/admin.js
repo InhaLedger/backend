3a900e8591e5e99837a6c88720db5c1dd9b47472
whitemark <whtmdgus56@nate.com>
2022-12-04 22:08:21 +0900

    feat: Add admin_finalized function

--- router/admin.js
+++ router/admin.js
@@ -108,20 +108,26 @@ router.get('/admin_proposal', auth, async (req,res) => {
 router.post('/admin_finalize', auth, async (req,res) => {
     if (admin == 0)
         return res.sendStatus(401)
-    // try {
-    //     const {amounts}  = req.body
-    //     postdata = {"amounts":amounts}
-    //     const response = await axios.post("http://211.226.199.46/coins/new", postdata)
-        
-    //     if (response.status == 204)
-    //         return res.sendStatus(201)
-    //     else
-    //         return res.sendStatus(500)
-    // }
-    // catch (err) {
-    //     console.log(err)
-    //     return res.sendStatus(400)
-    // }
+    try {
+        prog_count = await query2(`select count(*) as prog from proposal where proposal_status='PROGRESS'`,[])
+        prog_count = prog_count[0]['prog']
+
+        for (i = 0; i<prog_count; i++) {
+            postdata = {"userId":"admin", "timestamp": Date.now(), "rewardPerProposal":20, "batchSize":1}
+            const response = await axios.put("http://211.226.199.46/proposals", postdata)
+            if (response.status == 200 && atob(response.data)=='1') {
+                const propid = await query2(`select proposal_id from proposal where proposal_status='PROGRESS' order by proposal_timeStamp limit 1`,[])
+                const done = await query2(`update proposal SET proposal_status='DONE' where proposal_id=?`,[propid[0]['proposal_id']])
+            }
+            else
+                return res.sendStatus(500)
+        }
+        return res.sendStatus(200)
+    }
+    catch (err) {
+        console.log(err)
+        return res.sendStatus(400)
+    }
 })
 
 module.exports = router
